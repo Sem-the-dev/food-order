c3467da1a9dba889c8cf0274e19993e86f80802a
sem-the-dev <semhar.tesfu@outlook.com>
2021-08-10 17:49:09 +0100

    add admin page

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,8 +4,11 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Admin</h1>
+<!--button to add admin-->
+        <a href="#" class="btn-primary">Add admin</a>
+        <br/><br/><br/>
 
-        <table>
+        <table class="tbl-full">
             <tr>
                 <th>S.N</th>
                 <th>Full name</th>
@@ -18,8 +21,26 @@
                 <td>Semhar Tesfu</td>
                 <td>Semmy</td>
                 <td>
-                    Update
-                    Delete
+                    <a href="#" class="btn-update-admin">Update Admin</a>
+                    <a href="#" class="btn-delete-admin">Delete Admin</a>
+                </td>
+            </tr>
+            <tr>
+                <td>2. </td>
+                <td>Semhar Tesfu</td>
+                <td>Semmy</td>
+                <td>
+                    <a href="#" class="btn-update-admin">Update Admin</a>
+                    <a href="#" class="btn-delete-admin">Delete Admin</a>
+                </td>
+            </tr>
+            <tr>
+                <td>3. </td>
+                <td>Semhar Tesfu</td>
+                <td>Semmy</td>
+                <td>
+                    <a href="#" class="btn-update-admin">Update Admin</a>
+                    <a href="#" class="btn-delete-admin">Delete Admin</a>
                 </td>
             </tr>
         </table>
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -3,6 +3,47 @@
 <div class="main-content">
     <div class="wrapper">
     <h1>Manage Food</h1>
+        <a href="#" class="btn-primary">Add admin</a>
+        <br/><br/><br/>
+
+        <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1. </td>
+                <td>Semhar Tesfu</td>
+                <td>Semmy</td>
+                <td>
+                    <a href="#" class="btn-update-admin">Update Admin</a>
+                    <a href="#" class="btn-delete-admin">Delete Admin</a>
+                </td>
+            </tr>
+            <tr>
+                <td>2. </td>
+                <td>Semhar Tesfu</td>
+                <td>Semmy</td>
+                <td>
+                    <a href="#" class="btn-update-admin">Update Admin</a>
+                    <a href="#" class="btn-delete-admin">Delete Admin</a>
+                </td>
+            </tr>
+            <tr>
+                <td>3. </td>
+                <td>Semhar Tesfu</td>
+                <td>Semmy</td>
+                <td>
+                    <a href="#" class="btn-update-admin">Update Admin</a>
+                    <a href="#" class="btn-delete-admin">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
 </div>
 </div>
 
